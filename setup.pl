9bd1140406c5f7c7cd2e35d13a6d7ec4dd18f7f0
Ethan House <ewh2048@rit.edu>
2013-03-09 04:17:59 -0500

    Removed extra line

--- setup.pl
+++ setup.pl
@@ -6,7 +6,6 @@ my $user = $ENV{'LOGNAME'};
 my $homedir = $ENV{'HOME'};
 my $type = $ARGV[1];
 
-system("mkdir $homedir/dotfiles");
 system("git clone git://github.com/ehouse/dotfiles.git $homedir/dotfiles");
 system("curl http://j.mp/spf13-vim3 -L > spf13-vim.sh && sh spf13-vim.sh");
 system("curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh");
