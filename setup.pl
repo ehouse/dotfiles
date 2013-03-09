72c5305af79a0a563489a518a889b6985457e713
Ethan House <ehouse@derpy.csh.rit.edu>
2013-03-09 03:13:29 -0500

    Finished setup script

---
+++ setup.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my $user = $ENV{'LOGNAME'};
+my $homedir = $ENV{'HOME'};
+my $type = $ARGV[1];
+
+system("mkdir $homedir/dotfiles");
+system("git clone git://github.com/ehouse/dotfiles.git $homedir/dotfiles");
+system("curl http://j.mp/spf13-vim3 -L > spf13-vim.sh && sh spf13-vim.sh");
+system("curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh");
+
+system("ln -s $homedir/dotfiles/vim/vimrc.local $homedir/.vimrc.local");
+system("ln -s $homedir/dotfiles/zsh/override.zsh $homedir/.oh-my-zsh/custom/override.zsh");
+
