b52e5128f3006e7dd96b9d8c2f0c165894aaa923
Ethan House <ehouse@exablox.com>
2013-08-21 17:09:42 -0700

    removed orphaned code of indeterminable purpose

--- setup.pl
+++ setup.pl
@@ -4,7 +4,6 @@ use warnings;
 
 my $user = $ENV{'LOGNAME'};
 my $homedir = $ENV{'HOME'};
-my $type = $ARGV[1];
 
 system("git clone git://github.com/ehouse/dotfiles.git $homedir/dotfiles");
 system("curl http://j.mp/spf13-vim3 -L > spf13-vim.sh && sh spf13-vim.sh");
